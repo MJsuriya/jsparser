5028bd9210cb91b933482cdd45638dd5fff46999
Jeya Suriya Muthumari <jeya.suriyam@emsibg.com>
2022-04-09 15:23:40 +0530

    Added nearley and sample grammar file

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,27 @@
 import type { NextPage } from "next";
+import Head from "next/head";
+import grammar from "../grammar/grammar";
 
 const Home: NextPage = () => {
+  const nearley = require("nearley");
+
+  const parser = new nearley.Parser(nearley.Grammar.fromCompiled(grammar));
+
+  // Parse something!
+  parser.feed("foo\n");
+
+  // parser.results is an array of possible parsings.
+  console.log(JSON.stringify(parser.results)); //
+
   return (
-    <div className="text-center text-3xl p-10 font-bold">
-      Welcome to jsParser
-    </div>
+    <>
+      <Head>
+        <title>jsParser</title>
+      </Head>
+      <div className="text-center text-3xl p-10 font-bold">
+        Welcome to jsParser
+      </div>
+    </>
   );
 };
 
